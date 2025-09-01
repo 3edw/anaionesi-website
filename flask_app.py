6b891642944f3ba7b6a95768cde18ff2eeef5b79
3edw <ana.ionesi@gmail.com>
2025-09-01 22:18:07 +0200

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Renders the main gallery page."""
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    """Renders the about me page."""
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
